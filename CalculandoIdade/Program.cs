faab03b02b17fef92a6eab5d513096dc69b56710
GlauberRoque <glauberroqueferr@gmail.com>
2021-06-24 22:40:57 -0300

    idade_commit

---
+++ CalculandoIdade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculandoIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a sua data de nascimento: ");
+            DateTime DataNasc = Convert.ToDateTime(Console.ReadLine());
+            
+            int idade = DateTime.Today.Year - DataNasc.Year;
+            
+                if (DateTime.Today.DayOfYear < DataNasc.DayOfYear)
+                {
+                    idade = idade - 1 ;
+                    Console.WriteLine(idade);           
+
+                }else{
+                    Console.WriteLine(idade);
+                }
+        }
+    }
+}
